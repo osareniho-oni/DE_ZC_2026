c905b48b0b9e13d0df9b446528311c13ccede5db
Gabriel <dlastdj10@gmail.com>
2026-02-06 10:13:49 -0700

    Added week 4

---
+++ 04-analytics-engineering/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-02-05T16:34:04.823121379+00:00
+-- finished_at: 2026-02-05T16:34:07.512734428+00:00
+-- elapsed: 2.7s
+-- outcome: success
+-- dialect: bigquery
+-- node_id: not available
+-- query_id: Z0z28j9TgJV1Sx2hN9YodSvctzt
+-- desc: not provided
+select 1 as id;
